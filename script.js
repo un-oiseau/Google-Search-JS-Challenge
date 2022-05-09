6de3587bff314a4d8d5a4cc8f4f1eeb634676339
Narine Harutyunyan <narharut208@gmail.com>
2022-05-10 01:22:06 +0400

    Fixed some broken img links

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 
 $(document).ready(function() {
 
-    // function getEveryNth(arr, searchResult) {
-    //     arr = $(".g.tF2Cxc");
-
-    //     searchResult = [];
-  
-    //     for (let i = 0; i < arr.length; i++) {
-    //         searchResult.push($(arr[i]).html());
-    //     }
-      
-    //     return searchResult;
-    //   }
-      
-    //   console.log(getEveryNth([3, 6, 9], 3));
-
-
-
     console.log(printObject( $('.g.tF2Cxc:nth-child(3n)') ) );
 
     function printObject( obj ) {
@@ -26,6 +10,20 @@ $(document).ready(function() {
     }
 
 });
+
+// function getEveryNth(arr, searchResult) {
+//     arr = $(".g.tF2Cxc");
+
+//     searchResult = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         searchResult.push($(arr[i]).innerHTML);
+//     }
+  
+//     return searchResult;
+//   }
+  
+//   console.log(getEveryNth(arr, 3));
 
     
 
